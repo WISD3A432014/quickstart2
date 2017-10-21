8557ca2a9699cd3e1cd28613f173467485544a94
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-22 00:08:20 +0800

    定義子視圖

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Bootstrap 樣板... -->
 
-                    You are logged in!
+    <div class="panel-body">
+        <!-- 顯示驗證錯誤 -->
+    @include('common.errors')
+
+    <!-- 新任務的表單 -->
+        <form action="/task" method="POST" class="form-horizontal">
+        {{ csrf_field() }}
+
+        <!-- 任務名稱 -->
+            <div class="form-group">
+                <label for="task-name" class="col-sm-3 control-label">任務</label>
+
+                <div class="col-sm-6">
+                    <input type="text" name="name" id="task-name" class="form-control">
                 </div>
             </div>
-        </div>
+
+            <!-- 增加任務按鈕-->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> 增加任務
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
-</div>
+
+    <!-- 代辦：目前任務 -->
 @endsection
