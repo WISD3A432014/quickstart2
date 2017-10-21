46442799dc0eb2a844a212ff033164872b7d4be8
Wu Hsiu Rong <mandy19970219@gm.student.ncut.edu.tw>
2017-10-22 00:38:02 +0800

    注入資源庫

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,19 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
     /**
-     * 建立一個新的控制器實例。
+     * 任務資源庫的實例。
      *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+    /**
+     * 建立新的控制器實例。
+     *
+     * @param  TaskRepository  $tasks
      * @return void
      */
-    public function __construct()
+    public function __construct(TaskRepository $tasks)
     {
         $this->middleware('auth');
+
+        $this->tasks = $tasks;
+    }
+
+    /**
+     * 取得給定使用者的所有任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
     }
 }
